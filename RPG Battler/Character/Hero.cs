481541903f3c4970974e708b1d9a77c3ffc06555
Gustavo Moreno <gustavoama14@gmail.com>
2025-03-21 22:24:38 -0600

    hero class change

--- RPG Battler/Character/Hero.cs
+++ RPG Battler/Character/Hero.cs
@@ -15,21 +15,71 @@ namespace RPG_Battler.Character
         public int Mana { get; set; }
         public int ExperienceRemaining { get; set; }
         public CombatClass CombatClass { get; set; }
-        public List<Item> Items { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<Spell> Spells { get; set; }
-        public List<Equipment> Equipment { get; set; }
+        public List<Item> Items { get; set; } = new List<Item>();
+        public List<Skill> Skills { get; set; } = new List<Skill>();
+        public List<Spell> Spells { get; set; } = new List<Spell>();
+        public List<Equipment> Equipment { get; set; } = new List<Equipment>();
 
         public Hero()
-        {       
+        {
+            Name = "Unknown";
+            Level = 0;
+            Health = Power = Luck = 1;
+            Mana = 0;
+            ExperienceRemaining = 100;
         }
 
         public void LevelUp()
         {
+            var generator = new Random();
+            Level++;
+
+            switch (CombatClass.ToString())
+            {
+                case "Mage":
+                    Health += generator.Next(1, 16);
+                    Power += generator.Next(3, 6);
+                    Luck += generator.Next(1, 4);
+                    break;
+                case "Warrior":
+                    Health += generator.Next(10, 21);
+                    Power += generator.Next(1, 4);
+                    Luck += generator.Next(1, 4);
+                    break;
+                case "Rogue":
+                    Health += generator.Next(1, 16);
+                    Power += generator.Next(1, 4);
+                    Luck += generator.Next(3, 6);
+                    break;
+            }
+
+            ExperienceRemaining = 100;
+            Console.WriteLine($"{Name} has reached Level {Level}. Strength increased!");
+        }
+
+        public void DisplayStats(bool showTotalStats = true)
+        {
+            Console.WriteLine($"\n*** {Name}'s Status ***");
+            Console.WriteLine($"{"Level:",-10} {Level}");
+            Console.WriteLine($"{"Health:",-10} {Health}");
+            Console.WriteLine($"{"Power:",-10} {Power}");
+            Console.WriteLine($"{"Luck:",-10} {Luck}");
+            Console.WriteLine($"{"Mana:",-10} {Mana}");
+
+            if (showTotalStats)
+            {
+                Console.WriteLine("\n[Total Stats with Equipment Bonuses Included]");
+            }
+            else
+            {
+                Console.WriteLine("\n[Base Stats Only]");
+            }
         }
 
         public void CalculateTotals()
         {
+            
         }
     }
 }
+
